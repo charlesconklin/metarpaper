5f0f16a892bf03032bdd00c5f42e7a193152a156
Charles Conklin <charles@cclsoftware.com>
2025-10-04 21:59:26 -0600

    .

--- paperdraw.py
+++ paperdraw.py
@@ -53,12 +53,12 @@ def drawMetar(metarInfo):
     #     if(num == 10):
     #         break
 
-    canvas.rectangle((10, 0, 260, 60), fill = epd.GRAY2)
+    canvas.rectangle((10, 0, 60, 260), fill = epd.GRAY2)
     canvas.text((10, 0), metarInfo["icaoId"], font = font35, fill = epd.GRAY4)
     canvas.text((10, 35), metarInfo["name"], font = font12, fill = epd.GRAY4)
-    updateImage = imageBase.crop([10, 0, 260, 60])
+    updateImage = imageBase.crop([10, 0, 60, 260])
     imageBase.paste(updateImage, (10, 0))
-    epd.display_Partial(epd.getbuffer(imageBase), 10, 0, 260, 60)
+    epd.display_Partial(epd.getbuffer(imageBase), 10, 0, 60, 260)
     #epd.display_Partial(epd.getbuffer(imageBase), 0, epd.height, 60, epd.height - 10)
     
     #epd.display_Fast(epd.getbuffer(imageBase))
