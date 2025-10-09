49432627c821c8a8e22dd06d153fd699dd1a87e8
Charles Conklin <charles@cclsoftware.com>
2025-10-09 17:23:32 -0600

    .

--- paperdraw.py
+++ paperdraw.py
@@ -128,9 +128,9 @@ def drawMetar(metarInfo):
     canvas.text((10, 35), icaoName, font = font12, fill = epd.GRAY4)
     # draw split line
     canvas.line(( epd.height/2, 50,  epd.height/2,  epd.width), fill = epd.GRAY4)    
-    epd.display_Fast(epd.getbuffer(imageBase))
     # winds
     canvas.text((10, 0), windDesc, font = font12, fill = epd.GRAY4)
+    epd.display_Fast(epd.getbuffer(imageBase))
     
 
 
