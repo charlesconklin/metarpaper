210605d13ddb249e4f5df8b7c9a122c782cbca18
Charles Conklin <charles@cclsoftware.com>
2025-10-09 17:03:37 -0600

    .

--- paperdraw.py
+++ paperdraw.py
@@ -133,6 +133,9 @@ def drawMetar(metarInfo):
     updateImage = imageBase.crop([10, 0, 60, 263])
     imageBase.paste(updateImage, (10, 0))
     epd.display_Partial(epd.getbuffer(imageBase), 10, 0, 60, 263)
+
+    canvas.rectangle((10, 61, 120, 263), fill = epd.GRAY4)
+    
     
     #epd.display_Fast(epd.getbuffer(imageBase))
 
