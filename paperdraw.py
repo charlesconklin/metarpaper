45ba38ceae94bd5baf109967ee19c859fe929dc8
Charles Conklin <charles@cclsoftware.com>
2025-10-09 21:18:38 -0600

    a

--- paperdraw.py
+++ paperdraw.py
@@ -142,22 +142,22 @@ def drawMetar(metarInfo):
     canvas.text((10, 90), f"Altimeter: {altimeterDesc}", font = font12, fill = epd.GRAY4)
     # weather
     offset = 0
-    rightset = 50
+    rightset = 0
     if (currWeatherDesc != ""):
         offset += 12
         canvas.text((10, 102), f"Weather: {currWeatherDesc}", font = font12, fill = epd.GRAY4)    
     if len(cloudLayerDesc) > 0:
         for cloudLayer in cloudLayerDesc:
             if rightset > 0:
-                canvas.text((10, 102 + offset), f"Clouds: {cloudLayer}", font = font12, fill = epd.GRAY4)
-            else: 
                 canvas.text((10 + rightset, 102 + offset), f"{cloudLayer}", font = font12, fill = epd.GRAY4)
+            else: 
+                canvas.text((10, 102 + offset), f"Clouds: {cloudLayer}", font = font12, fill = epd.GRAY4)
+                rightset += 50
             offset += 12
-            rightset += 12
     else:
         canvas.text((10, 102 + offset), f"Clouds: {cloudSummaryDesc}", font = font12, fill = epd.GRAY4)
 
-    canvas.text((10, epd.width - 12), f"Observed at {timeDesc}", font = font12, fill = epd.GRAY4)
+    canvas.text((10, epd.width - 13), f"Observed at {timeDesc}", font = font12, fill = epd.GRAY4)
 
     epd.display_Fast(epd.getbuffer(imageBase))
     
