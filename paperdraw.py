e29d87d1d3feca0b6d337501692c39acf4650ead
Charles Conklin <charles@cclsoftware.com>
2025-11-07 15:40:58 -0700

    d

--- paperdraw.py
+++ paperdraw.py
@@ -189,7 +189,7 @@ def drawMetar(metarInfo):
     # epd.display_Partial(epd.getbuffer(Himage),110, epd.height - 120, 150, epd.height - 10)      
       newimage = imageBase.crop([1, 1, epd.width // 2, epd.height // 2])
       imageBase.paste(newimage, (1, 1))
-      epd.display_Partial(epd.getbuffer(imageBase), 1, 1, epd.width, epd.height)
+      epd.display_Partial(epd.getbuffer(imageBase), 1, 1, epd.width // 2, epd.height //2)
       
     #   newimage2 = imageBase.crop([epd.getbuffer(imageBase), (epd.height // 2) + 1, (epd.width // 2) + 1, epd.height - 2, epd.width - 2])
     #   imageBase.paste(newimage2, ((epd.width // 2) + 1, (epd.height // 2) + 1))
