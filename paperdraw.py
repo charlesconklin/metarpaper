e21fe3b34c998423dedb2697a2f757964de27345
Charles Conklin <charles@cclsoftware.com>
2025-11-07 15:02:45 -0700

    s

--- paperdraw.py
+++ paperdraw.py
@@ -189,13 +189,13 @@ def drawMetar(metarInfo):
     # newimage = Himage.crop([10, 110, 120, 150])
     # Himage.paste(newimage, (10,110)) 
     # epd.display_Partial(epd.getbuffer(Himage),110, epd.height - 120, 150, epd.height - 10)
-      newimage = imageBase.crop([1, 1, epd.width // 2, epd.height // 2])
+      newimage = imageBase.crop([1, 1, epd.height // 2, epd.width // 2])
       imageBase.paste(newimage, (1, 1))
-      epd.display_Partial(epd.getbuffer(imageBase), 1, 1, epd.width // 2, epd.height // 2)
+      epd.display_Partial(epd.getbuffer(imageBase), 1, 1, epd.height // 2, epd.width // 2)
       
-      newimage2 = imageBase.crop([epd.getbuffer(imageBase), (epd.width // 2) + 1, (epd.height // 2) + 1, epd.width - 2, epd.height - 2])
+      newimage2 = imageBase.crop([epd.getbuffer(imageBase), (epd.height // 2) + 1, (epd.width // 2) + 1, epd.height - 2, epd.width - 2])
       imageBase.paste(newimage2, ((epd.width // 2) + 1, (epd.height // 2) + 1))
-      epd.display_Partial(epd.getbuffer(imageBase), (epd.width // 2) + 1, (epd.height // 2) + 1, epd.width - 2, epd.height - 2)
+      epd.display_Partial(epd.getbuffer(imageBase), (epd.height // 2) + 1, (epd.width // 2) + 1, epd.height - 2, epd.width - 2)
       refreshIndex += 1
 
 
