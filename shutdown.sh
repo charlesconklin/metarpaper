456404f224c3e21b55b98904c0ce5fe1ac5d3896
Charles Conklin <charles@cclsoftware.com>
2025-11-18 21:36:16 -0700

    c

--- shutdown.sh
+++ shutdown.sh
@@ -1,4 +1,4 @@
 /usr/bin/sudo pkill -F /home/pi/metar/metarmap/metarpid.pid
 /usr/bin/sudo pkill -F /home/pi/metar/metarpaper/metarpaperpid.pid
 /usr/bin/sudo /usr/bin/python3 /home/pi/metar/metarmap/pixelsoff.py
-/usr/bin/sudo /usr/bin/python3 /home/pi/metar/metapaper/clearscreen.py
+/usr/bin/sudo /usr/bin/python3 /home/pi/metar/metarpaper/clearscreen.py
